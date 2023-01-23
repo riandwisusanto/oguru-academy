d00113a46a6e4fbe805281ca052a3a969977c4d5
Tony <tnybaskoro@gmail.com>
2023-01-23 17:17:05 +0700

    update route done

--- resources/js/landing/Index.vue
+++ resources/js/landing/Index.vue
@@ -1,7 +1,7 @@
 <template>
-    <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-    <![endif]-->
+        <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
 
     <!-- Add your site or application content here -->
 
@@ -10,9 +10,9 @@
         <div id="loading-center">
             <div id="loading-center-absolute">
                 <div class="loading-content">
-                    <img class="loading-logo-text" src="assets/img/logo/logo-text-2.png" alt="">
+                    <!--                    <img class="loading-logo-text" src="assets/img/logo/logo-text-2.png" alt="">-->
                     <div class="loading-stroke">
-                        <img class="loading-logo-icon" src="assets/img/logo/logo-icon.png" alt="">
+                        <!--                        <img class="loading-logo-icon" src="assets/img/logo/logo-icon.png" alt="">-->
                     </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@
                                 <nav>
                                     <ul>
                                         <li>
-                                            <a href="course-grid.html" class="cat-menu d-flex align-items-center">
+                                            <a href="/course" class="cat-menu d-flex align-items-center">
                                                 <div class="cat-dot-icon d-inline-block">
                                                     <svg viewBox="0 0 276.2 276.2">
                                                         <g>
@@ -91,7 +91,7 @@
                                             </ul>
                                         </li>
                                         <li class="has-dropdown">
-                                            <a href="course-grid.html">Courses</a>
+                                            <a href="/course">Courses</a>
                                             <ul class="submenu">
                                                 <li><a href="course-grid.html">Courses</a></li>
                                                 <li><a href="course-list.html">Course List</a></li>
@@ -949,120 +949,24 @@
                 <div class="row">
                     <div class="col-xxl-4 offset-xxl-4">
                         <div class="section__title-wrapper mb-60 text-center">
-                            <h2 class="section__title">Simple <br> All Inclusive  <span class="yellow-bg yellow-bg-big">Pricing<img src="assets/img/shape/yellow-bg.png" alt=""></span></h2>
+                            <h2 class="section__title">Our  <span class="yellow-bg yellow-bg-big">Partners<img src="assets/img/shape/yellow-bg.png" alt=""></span></h2>
                             <p>No contracts. No surprise fees.</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xxl-12">
-                        <div class="price__tab-btn text-center mb-50">
-                            <nav>
-                                <div class="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
-                                    <button class="nav-link" id="nav-monthly-tab" data-bs-toggle="tab" data-bs-target="#nav-monthly" type="button" role="tab" aria-controls="nav-monthly" aria-selected="true">monthly plan</button>
-                                    <button class="nav-link active" id="nav-annually-tab" data-bs-toggle="tab" data-bs-target="#nav-annually" type="button" role="tab" aria-controls="nav-annually" aria-selected="false">Annual Plan</button>
-                                </div>
-                            </nav>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-xxl-10 offset-xxl-1 col-xl-10 offset-xl-1 col-lg-10 offset-lg-1">
                         <div class="price__tab-content">
-                            <div class="tab-content" id="nav-tabContent">
-                                <div class="tab-pane fade" id="nav-monthly" role="tabpanel" aria-labelledby="nav-monthly-tab">
-                                    <div class="row">
-                                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                                            <div class="price__item grey-bg mb-30 p-relative">
-                                                <div class="price__head">
-                                                    <h3>Gold</h3>
-                                                    <p>Perfect for small marketing teams</p>
-                                                </div>
-                                                <div class="price__tag mb-25">
-                                                    <h4>$59<span>.99 / annually</span></h4>
-                                                </div>
-                                                <div class="price__features mb-40">
-                                                    <ul>
-                                                        <li><i class="far fa-check"></i>Course Discussions</li>
-                                                        <li><i class="far fa-check"></i>Content Library</li>
-                                                        <li><i class="far fa-check"></i>1-hour Mentorship</li>
-                                                    </ul>
-                                                </div>
-                                                <a href="contact.html" class="e-btn e-btn-4">Get Started</a>
-                                            </div>
-                                        </div>
-                                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                                            <div class="price__item grey-bg mb-30 p-relative">
-                                                <div class="price__offer">
-                                                    <span>Best Value</span>
-                                                </div>
-                                                <div class="price__head">
-                                                    <h3>Diamond</h3>
-                                                    <p>Perfect for small marketing teams</p>
-                                                </div>
-                                                <div class="price__tag mb-25">
-                                                    <h4>$99<span>.99 / annually</span></h4>
-                                                </div>
-                                                <div class="price__features mb-40">
-                                                    <ul>
-                                                        <li><i class="far fa-check"></i>Course Discussions</li>
-                                                        <li><i class="far fa-check"></i>Content Library</li>
-                                                        <li><i class="far fa-check"></i>1-hour Mentorship</li>
-                                                        <li><i class="far fa-check"></i>Online Course</li>
-                                                    </ul>
-                                                </div>
-                                                <a href="contact.html" class="e-btn e-btn-border">Get Started</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="tab-pane fade show active" id="nav-annually" role="tabpanel" aria-labelledby="nav-annually-tab">
-                                    <div class="row">
-                                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                                            <div class="price__item grey-bg mb-30 p-relative">
-                                                <div class="price__head">
-                                                    <h3>Gold</h3>
-                                                    <p>Perfect for small marketing teams</p>
-                                                </div>
-                                                <div class="price__tag mb-25">
-                                                    <h4>$59<span>.99 / annually</span></h4>
-                                                </div>
-                                                <div class="price__features mb-40">
-                                                    <ul>
-                                                        <li><i class="far fa-check"></i>Course Discussions</li>
-                                                        <li><i class="far fa-check"></i>Content Library</li>
-                                                        <li><i class="far fa-check"></i>1-hour Mentorship</li>
-                                                    </ul>
-                                                </div>
-                                                <a href="contact.html" class="e-btn e-btn-4">Get Started</a>
-                                            </div>
-                                        </div>
-                                        <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-6">
-                                            <div class="price__item grey-bg mb-30 p-relative">
-                                                <div class="price__offer">
-                                                    <span>Best Value</span>
-                                                </div>
-                                                <div class="price__head">
-                                                    <h3>Diamond</h3>
-                                                    <p>Perfect for small marketing teams</p>
-                                                </div>
-                                                <div class="price__tag mb-25">
-                                                    <h4>$99<span>.99 / annually</span></h4>
-                                                </div>
-                                                <div class="price__features mb-40">
-                                                    <ul>
-                                                        <li><i class="far fa-check"></i>Course Discussions</li>
-                                                        <li><i class="far fa-check"></i>Content Library</li>
-                                                        <li><i class="far fa-check"></i>1-hour Mentorship</li>
-                                                        <li><i class="far fa-check"></i>Online Course</li>
-                                                    </ul>
-                                                </div>
-                                                <a href="contact.html" class="e-btn e-btn-border">Get Started</a>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/1.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/2.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/3.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/4.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/5.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/6.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/7.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/8.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/9.jpg">
+                            <img class="m-4" style="max-height: 100px" src="assets/img/partner/10.jpg">
                         </div>
                     </div>
                 </div>
@@ -1204,10 +1108,9 @@
 
 <script>
 export default {
-    name: "index.vue"
+    name: 'Home',
 }
 </script>
-
 <style scoped>
 
 </style>
