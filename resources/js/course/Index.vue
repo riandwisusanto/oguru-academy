d392f023a514682fb0f9eeade69c3eaf1bc3fd08
Tony <tnybaskoro@gmail.com>
2023-01-27 14:57:26 +0700

    smol update

--- resources/js/course/Index.vue
+++ resources/js/course/Index.vue
@@ -6,18 +6,7 @@
     <!-- Add your site or application content here -->
 
     <!-- pre loader area start -->
-    <div id="loading">
-        <div id="loading-center">
-            <div id="loading-center-absolute">
-                <div class="loading-content">
-                    <img class="loading-logo-text" src="assets/img/logo/logo-text-2.png" alt="">
-                    <div class="loading-stroke">
-                        <img class="loading-logo-icon" src="assets/img/logo/logo-icon.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+
     <!-- pre loader area end -->
 
     <!-- back to top start -->
