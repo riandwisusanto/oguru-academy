203a1d09e3e3ae6b3860f689f07ae707dce1052f
Tony <tnybaskoro@gmail.com>
2023-01-27 16:39:04 +0700

    registration

---
+++ resources/js/registration/Index.vue
@@ -0,0 +1,256 @@
+<template>
+    <main>
+
+        <!-- page title area start -->
+        <section class="page__title-area page__title-height page__title-overlay d-flex align-items-center" data-background="assets/img/page-title/page-title-2.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xxl-12">
+                        <div class="page__title-wrapper mt-110">
+                            <h3 class="page__title">Registration</h3>
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page">Registration</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- page title area end -->
+
+        <!-- coupon-area start -->
+        <section class="coupon-area pt-100 pb-30">
+
+        </section>
+        <!-- coupon-area end -->
+
+        <!-- checkout-area start -->
+        <section class="checkout-area pb-70">
+            <div class="container">
+                <form action="#">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="checkbox-form">
+                                <h3>Registration</h3>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="country-select">
+                                            <label>Country <span class="required">*</span></label>
+                                            <select>
+                                                <option value="volvo">bangladesh</option>
+                                                <option value="saab">Algeria</option>
+                                                <option value="mercedes">Afghanistan</option>
+                                                <option value="audi">Ghana</option>
+                                                <option value="audi2">Albania</option>
+                                                <option value="audi3">Bahrain</option>
+                                                <option value="audi4">Colombia</option>
+                                                <option value="audi5">Dominican Republic</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="checkout-form-list">
+                                            <label>First Name <span class="required">*</span></label>
+                                            <input type="text" placeholder="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="checkout-form-list">
+                                            <label>Last Name <span class="required">*</span></label>
+                                            <input type="text" placeholder="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="checkout-form-list">
+                                            <label>Company Name</label>
+                                            <input type="text" placeholder="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="checkout-form-list">
+                                            <label>Address <span class="required">*</span></label>
+                                            <input type="text" placeholder="Street address" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="checkout-form-list">
+                                            <input type="text" placeholder="Apartment, suite, unit etc. (optional)" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="checkout-form-list">
+                                            <label>Town / City <span class="required">*</span></label>
+                                            <input type="text" placeholder="Town / City" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="checkout-form-list">
+                                            <label>State / County <span class="required">*</span></label>
+                                            <input type="text" placeholder="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="checkout-form-list">
+                                            <label>Postcode / Zip <span class="required">*</span></label>
+                                            <input type="text" placeholder="Postcode / Zip" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="checkout-form-list">
+                                            <label>Email Address <span class="required">*</span></label>
+                                            <input type="email" placeholder="" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="checkout-form-list">
+                                            <label>Phone <span class="required">*</span></label>
+                                            <input type="text" placeholder="Postcode / Zip" />
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="checkout-form-list">
+                                            <label>File <span class="required">*</span></label>
+                                            <input type="file" class="form-control"/>
+                                        </div>
+                                    </div>
+                                    <div class="order-notes">
+                                        <div class="checkout-form-list">
+                                            <label>Order Notes</label>
+                                            <textarea id="checkout-mess" cols="30" rows="10"
+                                                      placeholder="Notes about your order, e.g. special notes for delivery."></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="order-button-payment mt-20">
+                                        <button type="submit" class="e-btn e-btn-border">Submit <i class="far fa-check"></i> </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <!-- checkout-area end -->
+
+
+    </main>
+
+    <!-- footer area start -->
+    <footer>
+        <div class="footer__area footer-bg">
+            <div class="footer__top pt-90 pb-40">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xxl-3 col-xl-3 col-lg-3 col-md-4 col-sm-6">
+                            <div class="footer__widget mb-50">
+                                <div class="footer__widget-head mb-22">
+                                    <div class="footer__logo">
+                                        <a href="index.html">
+                                            <img src="assets/img/logo/logo-2.png" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="footer__widget-body">
+                                    <p>Great lesson ideas and lesson plans for ESL teachers! Educators can customize lesson plans to best.</p>
+
+                                    <div class="footer__social">
+                                        <ul>
+                                            <li><a href="#"><i class="social_facebook"></i></a></li>
+                                            <li><a href="#" class="tw"><i class="social_twitter"></i></a></li>
+                                            <li><a href="#" class="pin"><i class="social_pinterest"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xxl-2 offset-xxl-1 col-xl-2 offset-xl-1 col-lg-3 offset-lg-0 col-md-2 offset-md-1 col-sm-5 offset-sm-1">
+                            <div class="footer__widget mb-50">
+                                <div class="footer__widget-head mb-22">
+                                    <h3 class="footer__widget-title">Company</h3>
+                                </div>
+                                <div class="footer__widget-body">
+                                    <div class="footer__link">
+                                        <ul>
+                                            <li><a href="#">About</a></li>
+                                            <li><a href="#">Courses</a></li>
+                                            <li><a href="#">Events</a></li>
+                                            <li><a href="#">Instructor</a></li>
+                                            <li><a href="#">Career</a></li>
+                                            <li><a href="#">Become a Teacher</a></li>
+                                            <li><a href="#">Contact</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xxl-2 col-xl-2 col-lg-2 offset-lg-0 col-md-3 offset-md-1 col-sm-6">
+                            <div class="footer__widget mb-50">
+                                <div class="footer__widget-head mb-22">
+                                    <h3 class="footer__widget-title">Platform</h3>
+                                </div>
+                                <div class="footer__widget-body">
+                                    <div class="footer__link">
+                                        <ul>
+                                            <li><a href="#">Browse Library</a></li>
+                                            <li><a href="#">Library</a></li>
+                                            <li><a href="#">Partners</a></li>
+                                            <li><a href="#">News & Blogs</a></li>
+                                            <li><a href="#">FAQs</a></li>
+                                            <li><a href="#">Tutorials</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-5 col-sm-6">
+                            <div class="footer__widget footer__pl-70 mb-50">
+                                <div class="footer__widget-head mb-22">
+                                    <h3 class="footer__widget-title">Subscribe</h3>
+                                </div>
+                                <div class="footer__widget-body">
+                                    <div class="footer__subscribe">
+                                        <form action="#">
+                                            <div class="footer__subscribe-input mb-15">
+                                                <input type="email" placeholder="Your email address">
+                                                <button type="submit">
+                                                    <i class="far fa-arrow-right"></i>
+                                                    <i class="far fa-arrow-right"></i>
+                                                </button>
+                                            </div>
+                                        </form>
+                                        <p>Get the latest news and updates right at your inbox.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer__bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xxl-12">
+                            <div class="footer__copyright text-center">
+                                <p>© 2022 Educal, All Rights Reserved. Design By <a href="index.html">Theme Pure</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer area end -->
+</template>
+
+<script>
+export default {
+    name: "Index"
+}
+</script>
+
+<style scoped>
+
+</style>
